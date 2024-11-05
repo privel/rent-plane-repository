7b5fe1f4a87b191cf3ffaab41694c8646334540c
vspxd <olegm2006@bk.ru>
2024-11-05 15:38:55 +0500

    add calendar

--- bot/management/commands/bot.py
+++ bot/management/commands/bot.py
@@ -1,16 +1,57 @@
-import asyncio
-from django.core.management.base import BaseCommand
+import telebot
+from telebot import types
 from django.conf import settings
-from django.template import Context
-from telegram import Bot, Update, InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup
-from telegram.ext import ApplicationBuilder, MessageHandler, filters, CallbackContext, ContextTypes, CommandHandler, \
-    CallbackQueryHandler
-from asgiref.sync import sync_to_async
-
-from bot.management.commands.dep.commands import check_records_user
+from django.core.management.base import BaseCommand
+from telegram_bot_calendar import DetailedTelegramCalendar, LSTEP
+from datetime import date
 from bot.models import Message, Profile, Rent
 
+# Состояния пользователя
+STATE_MAIN_MENU = "MAIN_MENU"
 
+# бронирование полёта
+STATE_BOOK = "BOOK"
+STATE_BOOK_CHOOSE_PLANE = "BOOK_CHOOSE_PLANE"
+STATE_BOOK_CHOOSE_DATE_ON_FLY = "BOOK_CHOOSE_TIME_ON_FLY"
+
+# Регистрация полёта
+STATE_REGISTER_FLY = "REGISTER_FLY"
+
+
+STATE_SETTINGS = "SETTINGS"
+
+STATE_CHECK_RESERVATIONS = "CHECK_RESERVATIONS"
+STATE_RESERVATION_CHANGE_DELETE = "RESERVATION"
+
+bot = telebot.TeleBot(settings.TOKEN)
+
+# Словарь для хранения дат пользователей
+user_dates = {}
+
+# Словарь для хранения истории состояний пользователей
+user_state_stack = {}
+
+def push_user_state(user_id, state):
+    if user_id not in user_state_stack:
+        user_state_stack[user_id] = []
+    if state not in user_state_stack[user_id] :
+        user_state_stack[user_id].append(state)
+    # print(f"[DEBUG] PUSH: {user_id} -> {state} (stack: {user_state_stack[user_id]})")
+
+
+def pop_user_state(user_id):
+    if user_id in user_state_stack and user_state_stack[user_id]:
+        removed_state = user_state_stack[user_id].pop()
+        # print(f"[DEBUG] POP: {user_id} <- {removed_state} (stack: {user_state_stack[user_id]})")
+    return user_state_stack[user_id][-1] if user_state_stack[user_id] else STATE_MAIN_MENU
+
+
+def get_current_user_state(user_id):
+    return user_state_stack[user_id][-1] if user_id in user_state_stack and user_state_stack[
+        user_id] else STATE_MAIN_MENU
+
+
+# Декоратор для логирования ошибок
 def log_error(f):
     def inner(*args, **kwargs):
         try:
@@ -24,69 +65,137 @@ def log_error(f):
 
 
 @log_error
-async def do_echo(update: Update, context: CallbackContext):
-    chat_id = update.message.chat_id
-    text = update.message.text
+@bot.message_handler(commands=['start'])
+def start_command(message):
+    push_user_state(message.from_user.id, STATE_MAIN_MENU)
+    show_main_menu(message)
 
-    p, _ = await sync_to_async(Profile.objects.get_or_create)(
-        external_id=chat_id,
-        defaults={'name': update.message.from_user.username}
+
+def show_main_menu(message):
+    # Очищаем стек состояний, чтобы пользователь был точно на главном экране
+    user_state_stack[message.from_user.id] = [STATE_MAIN_MENU]
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add("Забронировать", "Настройки")
+    keyboard.add("Регистрация полёта")
+    keyboard.add("Проверить активные брони")
+    bot.send_message(message.chat.id, "Главное меню: Выберите операцию", reply_markup=keyboard)
+
+
+@bot.message_handler(func=lambda message: message.text == "Забронировать")
+def handle_book(message):
+    push_user_state(message.from_user.id, STATE_BOOK_CHOOSE_PLANE)
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add("Самолёт 1", "Самолёт 2")
+    keyboard.add("Назад")
+    bot.send_message(
+        message.chat.id,
+        "Вы выбрали 'Забронировать'. Процесс бронирования начат.\nВыберите самолёт\nНажмите 'Назад', чтобы вернуться.",
+        reply_markup=keyboard
     )
 
-    message_instance = Message(
-        profile=p,
-        text=text,
-    )
-    await sync_to_async(message_instance.save)()
 
-    reply_text = "Your ID = {}\n\n{}".format(chat_id, text)
-    await update.message.reply_text(text=reply_text)
 
-async def start_command(update: Update, context: CallbackContext) -> None:
-    reply_keyboard = [
-        ['Забронировать', 'Настройки'],
-        ['Проверить активные брони']
-    ]
-
-    await update.message.reply_text(
-        f"Привет {update.message.from_user.username}!\nВыберите операцию",
-        reply_markup=ReplyKeyboardMarkup(reply_keyboard, resize_keyboard=True, one_time_keyboard=True)
+@bot.message_handler(func=lambda message: message.text in ["Самолёт 1", "Самолёт 2"])
+def handle_book_choose_plane(message):
+    push_user_state(message.from_user.id, STATE_BOOK_CHOOSE_DATE_ON_FLY)
+    chosen_plane = message.text
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add("Назад")
+    bot.send_message(
+        message.chat.id,
+        f"Вы выбрали {chosen_plane}. Выбор сохранен.\nТеперь выберите дату полёта",
+        reply_markup=keyboard
     )
 
-
-async def handle_book(update: Update, context: CallbackContext) -> None:
-    await update.message.reply_text("Вы выбрали 'Забронировать'. Процесс бронирования начат.")
-
-async def handle_settings(update: Update, context: CallbackContext) -> None:
-    await update.message.reply_text("Вы выбрали 'Настройки'. Здесь можно изменить параметры.")
-
-async def handle_check_reservations(update: Update, context: CallbackContext) -> None:
-    await check_records_user(update)  # Вызов функции для проверки активных броней
+    calendar_command(message)
 
 
-async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    query = update.callback_query
+@bot.message_handler(func=lambda message: message.text == "Настройки")
+def handle_settings(message):
+    push_user_state(message.from_user.id, STATE_SETTINGS)
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add("Назад")
+    bot.send_message(message.chat.id, "Вы выбрали 'Настройки'. Нажмите 'Назад', чтобы вернуться.",
+                     reply_markup=keyboard)
 
-    await query.answer()
+@bot.message_handler(func=lambda message: message.text == "Назад")
+def handle_back(message):
+    # Получаем текущее состояние после удаления верхнего элемента стека
+    previous_state = pop_user_state(message.from_user.id)
 
-    await query.edit_message_text(text=f"Selected option: {query.data}")
+    # Отладочный вывод
+    #print(f"[DEBUG] BACK BUTTON: previous_state = {previous_state}")
+
+    # Проверяем состояние после возврата и выводим нужное меню
+    if previous_state == STATE_MAIN_MENU:
+        show_main_menu(message)
+
+
+    elif previous_state == STATE_BOOK:
+        handle_book(message)
+    elif previous_state == STATE_BOOK_CHOOSE_PLANE:
+        handle_book(message)  # Возврат на меню выбора самолета
+    elif previous_state == STATE_BOOK_CHOOSE_DATE_ON_FLY:
+        handle_book_choose_plane(message)  # Вернёт на выбор самолёта
+
+
+    elif previous_state == STATE_SETTINGS:
+        handle_settings(message)
+    else:
+        # Если состояние не определено, возвращаем на главный экран
+        show_main_menu(message)
+
+
+# @bot.message_handler(func=lambda message: message.text == "Настройки123")
+def calendar_command(message):
+    # push_user_state(message.from_user.id, STATE_BOOK_CHOOSE_DATE_ON_FLY)
+    current_year = date.today().year
+    calendar, step = DetailedTelegramCalendar(min_date=date(current_year, 1, 1),
+                                              current_date=date(current_year, 1, 1)).build()
+
+    bot.send_message(
+        message.chat.id,
+        f"Выберите {LSTEP[step]}",
+        reply_markup=calendar
+    )
+
+
+@bot.callback_query_handler(func=DetailedTelegramCalendar.func())
+def handle_calendar_selection(call):
+    result, key, step = DetailedTelegramCalendar(min_date=date.today()).process(call.data)
+
+    # Проверяем, выбрана ли допустимая дата
+    if result and result < date.today():
+        # Если дата в прошлом, сообщаем пользователю, что выбор недопустим
+        bot.answer_callback_query(call.id, "Нельзя выбрать дату в прошлом. Пожалуйста, выберите другую дату.")
+        # Оставляем календарь на текущем шаге
+        bot.edit_message_text(
+            f"Выберите {LSTEP[step]}",
+            chat_id=call.message.chat.id,
+            message_id=call.message.message_id,
+            reply_markup=key
+        )
+    elif not result and key:
+        # Оставляем календарь открытым, если выбор даты еще не завершен
+        bot.edit_message_text(
+            f"Выберите {LSTEP[step]}",
+            chat_id=call.message.chat.id,
+            message_id=call.message.message_id,
+            reply_markup=key
+        )
+    elif result:
+        # Сохраняем корректную дату
+        user_dates[call.from_user.id] = result
+        bot.edit_message_text(
+            f"Вы выбрали {result}. Дата сохранена!",
+            chat_id=call.message.chat.id,
+            message_id=call.message.message_id
+        )
+        push_user_state(call.from_user.id, STATE_MAIN_MENU)
 
 
 class Command(BaseCommand):
-    help = "Telegram Bot"
+    help = "Запуск Telegram бота"
 
     def handle(self, *args, **options):
-        application = ApplicationBuilder().token(settings.TOKEN).build()
-
-        application.add_handler(CommandHandler("start", start_command))
-
-        application.add_handler(CallbackQueryHandler(button))
-
-
-        application.add_handler(MessageHandler(filters.TEXT & filters.Regex("^Забронировать$"), handle_book))
-        application.add_handler(MessageHandler(filters.TEXT & filters.Regex("^Настройки$"), handle_settings))
-        application.add_handler(
-            MessageHandler(filters.TEXT & filters.Regex("^Проверить активные брони$"), handle_check_reservations))
-
-        # Запуск polling
-        application.run_polling()
+        bot.polling(none_stop=True)
--- bot/management/commands/dep/commands.py
+++ bot/management/commands/dep/commands.py
@@ -1,35 +1,41 @@
-from asgiref.sync import sync_to_async
+# from asgiref.sync import sync_to_async
+# from telegram.ext import CallbackContext
+#
+# from bot.models import Message, Profile, Rent
+# from telegram import Update, ReplyKeyboardMarkup
+#
+#
+# async def check_records_user(update: Update, context: CallbackContext):
+#     keyboard = [
+#         ['Изменить'],
+#         ['Назад'],
+#     ]
+#
+#     try:
+#         # Retrieve the profile asynchronously
+#         profile = await sync_to_async(Profile.objects.get)(external_id=update.message.chat_id)
+#
+#         # Retrieve all Rent records related to this profile
+#         rent_records = await sync_to_async(list)(Rent.objects.filter(profile=profile))
+#
+#         # Check if there are any records and construct the response
+#         if rent_records:
+#             reply_text = f"Активные брони:\n" + "\n".join(
+#                 f"Rent ID: {rent.pk},\n Aerodrom: {rent.aerodrom},\n Created At: {rent.created_at} \n\n"
+#                 for rent in rent_records
+#             )
+#         else:
+#             reply_text = "Нет активных записей"
+#
+#     except Profile.DoesNotExist:
+#         reply_text = "Профиль не найден"
+#
+#     await update.message.reply_text(reply_text,
+#                                     reply_markup=ReplyKeyboardMarkup(
+#                                         keyboard,
+#                                         resize_keyboard=True,
+#                                         one_time_keyboard=True), )
 
-from bot.models import Message, Profile, Rent
-from telegram import Update, ReplyKeyboardMarkup
 
 
-async def check_records_user(update: Update):
-    keyboard = [
-        ['Изменить'],
-    ]
 
-    try:
-        # Retrieve the profile asynchronously
-        profile = await sync_to_async(Profile.objects.get)(external_id=update.message.chat_id)
-
-        # Retrieve all Rent records related to this profile
-        rent_records = await sync_to_async(list)(Rent.objects.filter(profile=profile))
-
-        # Check if there are any records and construct the response
-        if rent_records:
-            reply_text = "Active bookings:\n" + "\n".join(
-                f"Rent ID: {rent.pk}, Aerodrom: {rent.aerodrom}, Created At: {rent.created_at}"
-                for rent in rent_records
-            )
-        else:
-            reply_text = "No active bookings found for this user."
-
-    except Profile.DoesNotExist:
-        reply_text = "Profile not found."
-
-    await update.message.reply_text(reply_text,
-                                    reply_markup=ReplyKeyboardMarkup(
-                                        keyboard,
-                                        resize_keyboard=True,
-                                        one_time_keyboard=True), )
